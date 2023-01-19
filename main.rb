eb6662017d17f7f67711d8530d78118ce55f5822
fmikolic <mikolicfran@gmail.com>
2023-01-19 14:24:16 +0100

    Added check if image is created, 1 todo for common side request

--- main.rb
+++ main.rb
@@ -39,11 +39,12 @@ class Main
         puts "-----------------------------------------------------------------------------------------------------"
         puts "Your choice: "
         input = gets.chomp.split(" ")
-        
+        if input.empty?
+            break
+        end
         
         case input[0].upcase
         
-        #need check if matrix is created before doing other operations
         when "I"
            if (1..250).include?(input[1].to_i) && (1..250).include?(input[2].to_i)
             @@m = Matrix.build(input[2].to_i, input[1].to_i) { |row, column| "0" }
@@ -51,43 +52,74 @@ class Main
             puts "Out of range (1-250)!"
            end
         when "L"
-            @@m[input[2].to_i - 1, input[1].to_i - 1] = input[3].to_s
+            if @@m.nil?
+                puts "Create image with I command"
+            else
+                @@m[input[2].to_i - 1, input[1].to_i - 1] = input[3].to_s
+            end
         when "V"
-            @@m.each_with_index do |e, row, col|
-                if col == input[1].to_i 
-                    if row >= input[2].to_i
-                        if row <= input[3].to_i
-                            @@m[row-1,col-1] = input[4].to_s
+            if @@m.nil?
+                puts "Create image with I command"
+            else
+                @@m.each_with_index do |e, row, col|
+                    if col == input[1].to_i 
+                        if row >= input[2].to_i
+                            if row <= input[3].to_i
+                                @@m[row-1,col-1] = input[4].to_s
+                            end
                         end
                     end
                 end
             end
 
         when "H"
-            @@m.each_with_index do |e, row, col|
-                if row == input[3].to_i 
-                    if col >= input[1].to_i
-                        if col <= input[2].to_i
-                            @@m[row-1,col-1] = input[4].to_s
+            if @@m.nil?
+                puts "Create image with I command"
+            else
+                @@m.each_with_index do |e, row, col|
+                    if row == input[3].to_i 
+                        if col >= input[1].to_i
+                            if col <= input[2].to_i
+                                @@m[row-1,col-1] = input[4].to_s
+                            end
                         end
                     end
                 end
             end
         when "F"
-            original_colour = @@m[input[2].to_i - 1, input[1].to_i - 1]
-            @@m[input[2].to_i - 1, input[1].to_i - 1] = input[3].to_s
-            #need rework for common side request
-            @@m.each_with_index do |e, row, col|
-                if (e == original_colour)
-                    @@m[row,col] = input[3].to_s
+            if @@m.nil?
+                puts "Create image with I command"
+            else
+                original_colour = @@m[input[2].to_i - 1, input[1].to_i - 1]
+                @@m[input[2].to_i - 1, input[1].to_i - 1] = input[3].to_s
+                #need rework for common side request
+                    
+                # @@m.to_a.each do |a|
+                    
+                #     p a
+                # end
+
+                @@m.each_with_index do |e, row, col|
+                    if e == original_colour
+                        @@m[row,col] = input[3].to_s
+                    end
                 end
             end
         when "C"
-            @@m.each_with_index do |e, row, col|
-                @@m[row,col] = "0"
+            if @@m.nil?
+                puts "Create image with I command"
+            else
+                @@m.each_with_index do |e, row, col|
+                    @@m[row,col] = "0"
+                end
             end
         when "S"
-            @@m.to_readable
+            if @@m.nil?
+                puts "Create image with I command"
+            else
+                @@m.to_readable
+                #p @@m.to_a
+            end
         when "X"
             exit
         else
